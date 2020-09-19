7664aa5bcd54e379da75982c531edf00fb1eeffa
PRABAKAR <58840015+prabha-coder@users.noreply.github.com>
2020-09-19 15:59:35 +0530

    Update prabha.js

--- prabha.js
+++ prabha.js
@@ -1,7 +1,7 @@
 var player = new Clappr.Player(
 		{
 			/*sources: ['http://185.8.176.227:25461/live/strtester/ap8s9ndf709a8/68.m3u8','http://randomserver.site:2086/crystal/12345/94780','http://185.8.176.227:25461/live/strtester/ap8s9ndf709a8/68.m3u8'],*/
-		source: 'http://randomserver.site:2086/wayne/1234/94792',
+		source: 'http://randomserver.site:2086/live/Bria/Bria/94780.m3u8',
 		mimeType:"application/x-mpegURL",
 		parentId: "#player",
 		autoPlay: true,
